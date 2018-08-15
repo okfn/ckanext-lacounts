3868ca5f7f270a3fabb31a1e84951dc91af31775
Sam Smith <sam@mintcanary.com>
2018-08-15 10:52:25 +0100

    search modal fixes

--- ckanext/lacounts/src/js/main.js
+++ ckanext/lacounts/src/js/main.js
@@ -9,11 +9,18 @@ $(document).ready(function(){
   // make the label / icon tabbable
   $('.masthead .site-search label').attr('tabindex', '0');
 
+  // toggle classes to hide search modal, and remove the close button
+  function closeSearch() {
+    $('.page').addClass( "inactive-search" );
+    $('.page').removeClass( "active-search" );
+    $( ".masthead .site-search .close" ).remove();
+  }
+
   // toggle classes to reveal search modal, add button to close it, and redirect the tabs
   function openSearch() {
     $('.page').addClass( "active-search" );
     $('.page').removeClass( "inactive-search" );
-    $(".site-search").append('<button class="close"><i class="fa fa-times" aria-hidden="true"></i><span class="sr-only">Close</span></button>');
+    $(".site-search").append('<button class="close" type="button"><i class="fa fa-times" aria-hidden="true"></i><span class="sr-only">Close</span></button>');
 
     // set first and last tabbable elements
     var firstInput = $('.active-search #field-sitewide-search');
@@ -34,14 +41,26 @@ $(document).ready(function(){
         lastInput.focus();
       }
     });
+
+    // close search on click
+    $( ".active-search .masthead .site-search .close" ).on( "click", function() {
+      closeSearch();
+    });
+
+    // close search on Enter
+    $( ".active-search .masthead .site-search .close" ).on( "keypress", function(e) {
+      if (e.which == 13) {
+        closeSearch();
+      }
+    });
   }
 
-  // do that on click
+  // open search on click
   $( ".inactive-search .masthead .site-search label" ).on( "click", function() {
     openSearch();
   });
 
-  // and on Enter
+  // open search on Enter
   $( ".inactive-search .masthead .site-search label" ).on( "keypress", function(e) {
     if (e.which == 13) {
       openSearch();
@@ -50,29 +69,6 @@ $(document).ready(function(){
   });
 
   // TODO: figure out why the above two calls run even when inactive-search is replaced with active-search on the .page element.
-
-  // toggle classes to hide search modal, and remove the close button
-  function closeSearch() {
-    $('.page').addClass( "inactive-search" );
-    $('.page').removeClass( "active-search" );
-    $( ".masthead .site-search .close" ).remove();
-  }
-
-  // do that on click
-  $( ".active-search .masthead .site-search .close" ).on( "click", function() {
-    closeSearch();
-  });
-
-  // and on Enter
-  $( ".active-search .masthead .site-search .close" ).on( "keypress", function(e) {
-    if (e.which == 13) {
-      closeSearch();
-    }
-  });
-
-  // TODO: figure out why the search form is being submitted when the close button is triggered.
-
-  // TODO: figure out why the above two calls don't work, if tried with a different element (not a button) to avoid submitting the form (see comment above).
 
 
   /* Header */
