60e6342b002d2f52bf2df7868e098660e5399e59
Sam Smith <sam@mintcanary.com>
2018-08-22 18:05:07 +0100

    (unstyled) carousel for tweets

--- ckanext/lacounts/src/js/home.js
+++ ckanext/lacounts/src/js/home.js
@@ -3,10 +3,31 @@ $(document).ready(function(){
   /* Twitter feed */
   /****************/
 
+  function handleTweets(tweets) {
+    var x = tweets.length;
+     var n = 0;
+     var element = document.getElementById('tweets');
+     var html = '<ul class="owl-carousel">';
+     while(n < x) {
+       html += '<li>' + tweets[n] + '</li>';
+       n++;
+     }
+     html += '</ul>';
+     element.innerHTML = html;
+
+     $(element).children("ul").owlCarousel({
+       items: 1,
+       autoHeight: true,
+       nav: true,
+       dots: false
+     });
+   }
+
   var tweetsHome = {
     "profile": {"screenName": 'LA_COUNTS'},
-    "domId": 'tweets',
-    "showInteraction": false
+    "domId": '',
+    "showInteraction": false,
+    "customCallback": handleTweets
   };
   twitterFetcher.fetch(tweetsHome);
 
