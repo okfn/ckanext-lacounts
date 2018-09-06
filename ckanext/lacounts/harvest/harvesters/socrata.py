f7fa170d76560bac742b5251e20a412c94273e23
Brook Elgie <specialbrew@gmail.com>
2018-09-06 16:49:42 +0100

    Don't need to override 'import_stage' in child class
    The superclass (ckanext-socrata) will call the process_package method
    directory. Subclasses are expected to override it.

--- ckanext/lacounts/harvest/harvesters/socrata.py
+++ ckanext/lacounts/harvest/harvesters/socrata.py
@@ -1,23 +1,8 @@
-import json
-
 from ckanext.socrata.plugin import SocrataHarvester
 from ckanext.lacounts.harvest import helpers
 
 
 class LacountsSocrataHarvester(SocrataHarvester):
-
-    def import_stage(self, harvest_object):
-        try:
-            package = json.loads(harvest_object.content)
-        except TypeError:
-            # harvest_object may not have content. For example, if set for
-            # deletion.
-            pass
-        else:
-            package = helpers.process_package(package, harvest_object)
-            harvest_object.content = json.dumps(package)
-        return super(LacountsSocrataHarvester, self) \
-            .import_stage(harvest_object)
 
     def process_package(self, package, harvest_object):
         package = helpers.process_package(package, harvest_object)
