92647e70ceca6f876667b45180b8dc20b2f3f4da
Brook Elgie <specialbrew@gmail.com>
2018-10-16 09:57:30 +0100

    Static pages controller
    Static pages for:
    - About
    - FAQs
    - Terms of Service
    - Privacy Policy
    - Resources
    
    Part of #101.

--- ckanext/lacounts/controller.py
+++ ckanext/lacounts/controller.py
@@ -14,12 +14,35 @@ class BlogController(ShowcaseController):
         showcases = toolkit.get_action('ckanext_showcase_list')(context, {})
         for showcase in showcases:
             for extra in showcase.get('extras', []):
-                if extra['key'] == 'story_type' and extra['value'] == 'Blog post':
+                if extra['key'] == 'story_type' \
+                   and extra['value'] == 'Blog post':
                     posts.append(showcase)
-        return toolkit.render(self._search_template('blog'), extra_vars={'posts': posts})
+        return toolkit.render(self._search_template('blog'),
+                              extra_vars={'posts': posts})
 
     def _search_template(self, package_type):
         return 'blog/search.html'
 
     def _read_template(self, package_type):
         return 'blog/read.html'
+
+
+class StaticController(toolkit.BaseController):
+
+    def faqs(self):
+        return toolkit.render('static/faqs.html')
+
+    def about(self):
+        return toolkit.render('static/about.html')
+
+    def termsofservice(self):
+        return toolkit.render('static/termsofservice.html')
+
+    def privacypolicy(self):
+        return toolkit.render('static/privacypolicy.html')
+
+    def resources(self):
+        return toolkit.render('static/resources.html')
+
+    def contact(self):
+        return toolkit.render('static/contact.html')
--- ckanext/lacounts/plugin.py
+++ ckanext/lacounts/plugin.py
@@ -65,6 +65,14 @@ class LacountsPlugin(plugins.SingletonPlugin, DefaultTranslation):
         with SubMapper(map, controller='ckanext.lacounts.controller:BlogController') as m:
             m.connect('blog_search', '/blog', action='search')
             m.connect('blog_read', '/blog/{id}', action='read')
+
+        with SubMapper(map, controller='ckanext.lacounts.controller:StaticController') as m:
+            m.connect('privacypolicy', '/privacy-policy', action='privacypolicy')
+            m.connect('termsofservice', '/terms-of-service', action='termsofservice')
+            m.connect('faqs', '/faqs', action='faqs')
+            m.connect('about', '/about', action='about')
+            m.connect('resources', '/resources', action='resources')
+            m.connect('contact', '/contact', action='contact')
         return map
 
     # IFacets
